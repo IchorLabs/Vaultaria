71ec70f6544880665486d687cec7c935e07929c4
Stinkalicious <blakercanter05@gmail.com>
2026-08-26 03:47:29 -0700

    trying again

--- Content/Items/Weapons/Ranged/Rare/Pistol/Jakobs/CyberEagle.cs
+++ Content/Items/Weapons/Ranged/Rare/Pistol/Jakobs/CyberEagle.cs
@@ -31,7 +31,7 @@ namespace Vaultaria.Content.Items.Weapons.Ranged.Rare.Pistol.Jakobs
             Item.noMelee = true;
             Item.shootSpeed = 10f;
             Item.shoot = ProjectileID.HeatRay;
-            Item.useAmmo = AmmoID.PistolAmmo;
+            Item.useAmmo = AmmoID.modcontent.itemtype<pistolAmmo>();
 
             // Combat properties
             Item.useStyle = ItemUseStyleID.Shoot;
