f6c5bc0b204b6eef32088c4b340efd0aacc657b0
Stinkalicious <blakercanter05@gmail.com>
2026-08-26 21:14:38 -0700

    this might be the last one

--- Content/Items/Weapons/Ranged/Effervescent/Pistol/Jakobs/Prototype2599.cs
+++ Content/Items/Weapons/Ranged/Effervescent/Pistol/Jakobs/Prototype2599.cs
@@ -157,7 +157,7 @@ namespace Vaultaria.Content.Items.Weapons.Ranged.Effervescent.Pistol.Jakobs
             ItemText.MultiShotText(tooltips, Item, 4);
             ItemText.Text(tooltips, Mod, "Tooltip1", "Uses Pistol Ammo\nFires as fast as you can pull the trigger... but not too fast");
             ItemText.RedText(tooltips, Mod, "Tooltip2", "An ode to Maxine");
-            ItemText.RedText(tooltips, Mod, "Tooltip3", "- Developer Note: This weapon is a tribute to my daughter.", ItemText.VaultarianColours.Healing);
+            ItemText.Text(tooltips, Mod, "Tooltip3", "- Developer Note: This weapon is a tribute to my daughter.", ItemText.VaultarianColours.Healing);
         }
     }
 }
